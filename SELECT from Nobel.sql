c637aedfa94ecd446a6dbaa43146fbd1f53b5c01
FahadAminShovon <bm.amin.fahad@gmail.com>
2019-12-10 19:03:48 +0600

    add queries in SELECT from Nobel.sql

--- SELECT from Nobel.sql
+++ SELECT from Nobel.sql
@@ -58,5 +58,32 @@ yr = 1980 AND subject <> 'Chemistry' AND subject <> 'Medicine'
 SELECT yr,subject,winner FROM nobel WHERE 
 (yr < 1910 AND subject = 'Medicine') OR (yr>=2004 AND subject = 'Literature')
 
+--11.
+--Find all details of the prize won by PETER GRÜNBERG 
+SELECT * FROM nobel 
+WHERE
+winner = 'PETER GRÜNBERG'
 
-	
+--12.
+--Find all details of the prize won by EUGENE O'NEILL 
+SELECT * FROM nobel 
+WHERE
+winner = 'EUGENE O\'NEILL'
+
+--13.
+--Knights in order
+--List the winners, year and subject where the winner starts
+--with Sir. Show the the most recent first, then by name order.
+SELECT winner,yr,subject FROM nobel WHERE
+winner LIKE 'SIR%' ORDER BY yr DESC, winner
+
+--14.
+--The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+--Show the 1984 winners and subject ordered by subject 
+--and winner name; but list Chemistry and Physics last.
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY
+CASE WHEN subject IN ('Chemistry','Physics') THEN 1 ELSE 0 END,subject,winner
+
